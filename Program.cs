a8d0300eed39e7922d519120ec202dd59735510e
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-22 15:46:36 +0530

    Add : Given 3 string find the maximum

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Generic Max Min Problem");
-            string str = "Amit";
-            string str1 = "Sagar";
-            string str2 = "Krishna";
+            //string str = "Amit";
+            //string str1 = "Sagar";
+            //string str2 = "Krishna";
 
-            string Max = Maximum.MaximumString(str, str1, str2);
-            Console.WriteLine(Max);
+            Maximum maximum = new Maximum();
+           string smax=maximum.FindMxString("kka","ll","mmii");
+            Console.WriteLine(smax + " is greatest string");
+            
+
 
 
 
