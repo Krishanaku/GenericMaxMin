1befd6c705b3ab6b0afb31aa5bc398946e395aae
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-22 15:46:30 +0530

    Add : Given 3 string find the maximum

--- Maximum.cs
+++ Maximum.cs
@@ -8,51 +8,69 @@ namespace Generics
 {
     public  class Maximum
     {
-        public static int MaximumString(int fNumber, int sNumber, int thirdNumber)
+        //public static string MaximumString(string fNumber, string sNumber, string thirdNumber)
+        //{
+        //    if ((fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) > 0) ||
+        //        (fNumber.CompareTo(sNumber) >= 0 && fNumber.CompareTo(thirdNumber) > 0) ||
+        //        (fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) >= 0))
+        //    {
+        //        return fNumber;
+        //    }
+
+        //    if ((sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) > 0) ||
+        //       (sNumber.CompareTo(fNumber) >= 0 && sNumber.CompareTo(thirdNumber) > 0) ||
+        //       (sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) >= 0))
+        //    {
+        //        return sNumber;
+        //    }
+
+
+        //    if ((thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) > 0) ||
+        //       (thirdNumber.CompareTo(fNumber) >= 0 && thirdNumber.CompareTo(sNumber) > 0) ||
+        //       (thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) >= 0))
+        //    {
+        //        return thirdNumber;
+        //    }
+        //    return fNumber;
+        //}
+
+        //public static string MaximumString(string first, string second, string third)
+        //{
+        //    if (first.Length > second.Length || first.Length > third.Length)
+        //    {
+        //        return "First is Greatest";
+        //    }
+        //    else if (second.Length > second.Length || second.Length > third.Length)
+        //    {
+        //        return "Second is Greatest";
+        //    }
+        //    else if (third.Length > first.Length || third.Length > second.Length)
+        //    {
+        //        return "Third is Greatest";
+        //    }
+        //    else
+        //    {
+        //        return "Every string is same";
+        //    }
+        //}
+
+        public string FindMxString(string First, string Second, string Third)
         {
-            if ((fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) > 0) ||
-                (fNumber.CompareTo(sNumber) >= 0 && fNumber.CompareTo(thirdNumber) > 0) ||
-                (fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) >= 0))
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
             {
-                return fNumber;
+                return First;
             }
-
-            if ((sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) > 0) ||
-               (sNumber.CompareTo(fNumber) >= 0 && sNumber.CompareTo(thirdNumber) > 0) ||
-               (sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) >= 0))
+            if (Second.CompareTo(Third) > 0 && Second.CompareTo(First) > 0)
             {
-                return sNumber;
+                return Second;
             }
-
-
-            if ((thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) > 0) ||
-               (thirdNumber.CompareTo(fNumber) >= 0 && thirdNumber.CompareTo(sNumber) > 0) ||
-               (thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) >= 0))
+            if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
             {
-                return thirdNumber;
+                return Third;
             }
-            return fNumber;
+            return First;
         }
 
-        public static string MaximumString(string first, string second, string third)
-        {
-            if (first.Length > second.Length || first.Length > third.Length)
-            {
-                return "First is Greatest";
-            }
-            else if (second.Length > second.Length || second.Length > third.Length)
-            {
-                return "Second is Greatest";
-            }
-            else if (third.Length > first.Length || third.Length > second.Length)
-            {
-                return "Third is Greatest";
-            }
-            else
-            {
-                return "Every string is same";
-            }
-        }
 
 
 
