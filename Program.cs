585c483ae5dd3d587013864fc5afebd3e7aaf7e7
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-22 14:00:10 +0530

    Add :  Display Welcome Max Min Generic Problem

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Generic Max Min Problem");
+
+
+
+
+        }
+    }
+}
