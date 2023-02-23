2518cc0320849199cb0834154c0d37bf39601bdf
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-23 15:51:32 +0530

    Add : Extend the max method to also print the max to std out using Generic Method

--- Program.cs
+++ Program.cs
@@ -4,14 +4,11 @@
     {
         public static void Main(string[] args) 
         {
-            Console.WriteLine("Welcome to Generic Max Min Problem");
-            int a = Maximum<int>.Maximum1(10, 20, 30);
-            double b = Maximum<double>.Maximum1(10.20, 20.03, 30.2);
-            char c = Maximum<char>.Maximum1('A', 'C', 'B');
-            Console.WriteLine("Greatest Int : " + a);
-            Console.WriteLine("Greatest Double : " + b);
-            Console.WriteLine("Greatest Char : " + c);
+            int[] arr = { 112, 344, 432, 555, 678 };
+            Maximum<int> generic = new Maximum<int>(arr);
+            generic.PrintMaxValue();
 
+    
 
         }
     }
