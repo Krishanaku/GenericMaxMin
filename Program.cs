d68f0ebbc18cad67d8f30b9afb8b1dc5c712d8ec
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-23 13:40:26 +0530

    Add : Refactor all the 3 to One Generic Method and find the maximum

--- Program.cs
+++ Program.cs
@@ -2,43 +2,15 @@
 {
     public class Program
     {
-        public static void toprint<T>(T[] intputArray)
-            //<T> is generic inilization
-            //T[] any type of data want to store
-            //var is used beacuse u store diff data type so u can use var
-
-        {
-            foreach (var i in intputArray)
-            {
-                Console.WriteLine(i);
-            }
-        }
-     
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Generic Max Min Problem");
-            //string str = "Amit";
-            //string str1 = "Sagar";
-            //string str2 = "Krishna";
-
-
-            int[] intArray = { 1, 2, 3, 4, 5};
-            double[] doubleArray = { 1.1,2.1,3.1,4.1};
-            char[] charArray = { 'a', 'k', 'l', 'n' };
-
-            Program.toprint<int>(intArray);
-            Program.toprint<double>(doubleArray);
-            Program.toprint<char>(charArray);
-
-
-
-            
-
-
-
-            
-
-
+            int a = Maximum<int>.Maximum1(10, 20, 30);
+            double b = Maximum<double>.Maximum1(10.20, 20.03, 30.2);
+            char c = Maximum<char>.Maximum1('A', 'C', 'B');
+            Console.WriteLine("Greatest Int : " + a);
+            Console.WriteLine("Greatest Double : " + b);
+            Console.WriteLine("Greatest Char : " + c);
 
 
         }
