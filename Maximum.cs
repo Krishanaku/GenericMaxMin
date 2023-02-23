d583ac6e3d7a056ad3128d38ca9947b79754534c
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-23 13:40:45 +0530

    Add : Refactor all the 3 to One Generic Method and find the maximum

--- Maximum.cs
+++ Maximum.cs
@@ -6,71 +6,41 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    public  class Maximum
+    public class Maximum<T> where T : IComparable
     {
-        //public static string MaximumString(string fNumber, string sNumber, string thirdNumber)
-        //{
-        //    if ((fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) > 0) ||
-        //        (fNumber.CompareTo(sNumber) >= 0 && fNumber.CompareTo(thirdNumber) > 0) ||
-        //        (fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) >= 0))
-        //    {
-        //        return fNumber;
-        //    }
+        public T fNumber, sNumber, thirdNumber;
 
-        //    if ((sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) > 0) ||
-        //       (sNumber.CompareTo(fNumber) >= 0 && sNumber.CompareTo(thirdNumber) > 0) ||
-        //       (sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) >= 0))
-        //    {
-        //        return sNumber;
-        //    }
-
-
-        //    if ((thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) > 0) ||
-        //       (thirdNumber.CompareTo(fNumber) >= 0 && thirdNumber.CompareTo(sNumber) > 0) ||
-        //       (thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) >= 0))
-        //    {
-        //        return thirdNumber;
-        //    }
-        //    return fNumber;
-        //}
-
-        //public static string MaximumString(string first, string second, string third)
-        //{
-        //    if (first.Length > second.Length || first.Length > third.Length)
-        //    {
-        //        return "First is Greatest";
-        //    }
-        //    else if (second.Length > second.Length || second.Length > third.Length)
-        //    {
-        //        return "Second is Greatest";
-        //    }
-        //    else if (third.Length > first.Length || third.Length > second.Length)
-        //    {
-        //        return "Third is Greatest";
-        //    }
-        //    else
-        //    {
-        //        return "Every string is same";
-        //    }
-        //}
-
-        public string FindMxString(string First, string Second, string Third)
+        public Maximum(T fNumber, T sNumber, T thirdNumber)
         {
-            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
-            {
-                return First;
-            }
-            if (Second.CompareTo(Third) > 0 && Second.CompareTo(First) > 0)
-            {
-                return Second;
-            }
-            if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
-            {
-                return Third;
-            }
-            return First;
+            this.fNumber = fNumber;
+            this.sNumber = sNumber;
+            this.thirdNumber = thirdNumber;
         }
+        public static T Maximum1(T fNumber, T sNumber, T thirdNumber)
+        {
+            if ((fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) > 0) ||
+                (fNumber.CompareTo(sNumber) >= 0 && fNumber.CompareTo(thirdNumber) > 0) ||
+                (fNumber.CompareTo(sNumber) > 0 && fNumber.CompareTo(thirdNumber) >= 0))
+            {
+                return fNumber;
+            }
 
+            if ((sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) > 0) ||
+               (sNumber.CompareTo(fNumber) >= 0 && sNumber.CompareTo(thirdNumber) > 0) ||
+               (sNumber.CompareTo(fNumber) > 0 && sNumber.CompareTo(thirdNumber) >= 0))
+            {
+                return sNumber;
+            }
+
+
+            if ((thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) > 0) ||
+               (thirdNumber.CompareTo(fNumber) >= 0 && thirdNumber.CompareTo(sNumber) > 0) ||
+               (thirdNumber.CompareTo(fNumber) > 0 && thirdNumber.CompareTo(sNumber) >= 0))
+            {
+                return thirdNumber;
+            }
+            return fNumber;
+        }
 
 
 
